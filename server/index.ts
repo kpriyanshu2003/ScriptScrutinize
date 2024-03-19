7c8bfa0ac5382a5bbef47f92489dcc72e2e4df94
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-03-19 20:18:29 +0530

    Initial Commit

---
+++ server/index.ts
@@ -0,0 +1,30 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import compres from "compression";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+dotenv.config();
+const app: Application = express();
+const port = process.env.PORT || 3000;
+export const prisma = new PrismaClient();
+
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+};
+
+app.use(cors(corsConfig));
+app.use(morgan("dev"));
+app.use(compres());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
